dfc566e1999131238fefca9811baa706fa027015
ageagaega <agegaegageaseg@gaoeinaoeign.com>
2015-07-15 14:42:24 -0400

    Update and rename README.md to test.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# woooo
-wooooo
---
+++ test.js
@@ -0,0 +1,15 @@
+/**
+ * In fact, you're looking at ACE right now. Go ahead and play with it!
+ *
+ * We are currently showing off the JavaScript mode. ACE has support for 45
+ * language modes and 24 color themes!
+ */
+
+function add(x, y) {
+    var resultString = "Hello, ACE! The result of your math is: ";
+    var result = x + y;
+    return resultString + result;
+}
+
+var addResult = add(3, 2);
+console.log(addResult);
